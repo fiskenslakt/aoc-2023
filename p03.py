dccf265c9a99cc9db1dedfc5a846a7f8bf3151de
Derek <sjukledighet@gmail.com>
2023-12-03 02:20:34 -0500

    Day 3 part 2

--- p03.py
+++ p03.py
@@ -14,7 +14,7 @@ from aocd import data, submit
 # .664.598..'''
 
 # import pudb;pu.db
-
+neighbors = ((0,1), (1,0), (1, 1), (-1, 0), (0, -1), (-1, -1), (1,-1), (-1, 1))
 part_numbers = []
 grid = data.splitlines()
 schematic_width = len(grid[0])
@@ -25,7 +25,7 @@ for m in re.finditer('\d+', data.replace('\n', '')):
     y = m.start() // schematic_width
     x = m.start() % schematic_width
     for offset in range(m.end() - m.start()):
-        for i, j in ((0,1), (1,0), (1, 1), (-1, 0), (0, -1), (-1, -1), (1,-1), (-1, 1)):
+        for i, j in neighbors:
             nx = x + i + offset
             ny = y + j
 
@@ -35,7 +35,7 @@ for m in re.finditer('\d+', data.replace('\n', '')):
                 continue
 
             if not (cell := grid[ny][nx]).isdigit() and cell != '.':
-                part_numbers.append(int(m[0]))
+                part_numbers.append(m)
                 break
         else:
             continue
@@ -43,4 +43,31 @@ for m in re.finditer('\d+', data.replace('\n', '')):
 
 # print(part_numbers)
 # print(sum(part_numbers))
-submit(sum(part_numbers))
+pn_coords = {}
+for pn in part_numbers:
+    pn_y = pn.start() // schematic_width
+    pn_x = pn.start() % schematic_height
+    for offset in range(pn.end() - pn.start()):
+        pn_coords[(pn_x+offset, pn_y)] = pn
+
+gear_ratios = []
+
+for m in re.finditer('\*', data.replace('\n', '')):
+    y = m.start() // schematic_width
+    x = m.start() % schematic_height
+
+    adj = set()
+
+    for i, j in neighbors:
+        nx = x + i
+        ny = y + j
+
+        if (nx, ny) in pn_coords:
+            adj.add(pn_coords[(nx, ny)])
+
+    if len(adj) == 2:
+        ratio = int(adj.pop()[0]) * int(adj.pop()[0])
+        gear_ratios.append(ratio)
+
+# print(gear_ratios)
+submit(sum(gear_ratios))
