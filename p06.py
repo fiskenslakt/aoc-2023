24ff2eac29fb9c78c1961e7351e09decbd3ac74b
Derek <sjukledighet@gmail.com>
2023-12-06 00:39:06 -0500

    Day 6 part 2

--- p06.py
+++ p06.py
@@ -1,21 +1,51 @@
+from math import prod
+
 from aocd import data, submit
 
+# data = '''Time:      7  15   30
+# Distance:  9  40  200'''
 
 times, dists = data.splitlines()
 
-times = list(map(int, times.split()[1:]))
-dists = list(map(int, dists.split()[1:]))
+real_t = int(''.join(times.split()[1:]))
+real_d = int(''.join(dists.split()[1:]))
+times = map(int, times.split()[1:])
+dists = map(int, dists.split()[1:])
 
-# print(times)
-# print(dists)
-x = []
-for time, dist in zip(times, dists):
-    y = 0
-    for i in range(1, time):
-        if i * (time - i) > dist:
-            y += 1
+# print(real_t)
+# print(real_d)
 
-    x.append(y)
+# x = []
+# for time, dist in zip(times, dists):
+#     y = 0
+#     for i in range(1, time):
+#         if i * (time - i) > dist:
+#             y += 1
 
-from math import prod
-submit(prod(x))
+#     x.append(y)
+
+
+# submit(prod(x))
+
+ways = 0
+for i in range(1, real_t):
+    if i * (real_t - i) > real_d:
+        ways += 1
+
+print(ways)
+submit(ways)
+raise SystemExit
+
+x = y = real_t // 2
+ways = 0
+while True:
+    # print(x, y, x * y)
+    if x * y > real_d:
+        ways += 1
+    else:
+        break
+
+    x += 1
+    y -= 1
+
+print(ways)
