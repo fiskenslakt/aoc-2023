9eedc8bb4522d527b9f7c7d5291bccfbdaa55c15
Derek <sjukledighet@gmail.com>
2023-12-06 02:39:12 -0500

    Day 6 final solution

--- p06.py
+++ p06.py
@@ -1,9 +1,6 @@
 from math import prod
 
-from aocd import data, submit
-
-# data = '''Time:      7  15   30
-# Distance:  9  40  200'''
+from aocd import data
 
 times, dists = data.splitlines()
 
@@ -12,40 +9,17 @@ real_d = int(''.join(dists.split()[1:]))
 times = map(int, times.split()[1:])
 dists = map(int, dists.split()[1:])
 
-# print(real_t)
-# print(real_d)
+total_ways = []
+for time, dist in zip(times, dists):
+    ways = 0
+    for i in range(1, time):
+        ways += i * (time - i) > dist
 
-# x = []
-# for time, dist in zip(times, dists):
-#     y = 0
-#     for i in range(1, time):
-#         if i * (time - i) > dist:
-#             y += 1
-
-#     x.append(y)
-
-
-# submit(prod(x))
+    total_ways.append(ways)
 
 ways = 0
 for i in range(1, real_t):
-    if i * (real_t - i) > real_d:
-        ways += 1
+    ways += i * (real_t - i) > real_d
 
-print(ways)
-submit(ways)
-raise SystemExit
-
-x = y = real_t // 2
-ways = 0
-while True:
-    # print(x, y, x * y)
-    if x * y > real_d:
-        ways += 1
-    else:
-        break
-
-    x += 1
-    y -= 1
-
-print(ways)
+print('Part 1:', prod(total_ways))
+print('Part 2:', ways)
