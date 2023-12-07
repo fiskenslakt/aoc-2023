44562d1935b34a240623627b7947ec94b93a5875
Derek <sjukledighet@gmail.com>
2023-12-07 18:47:22 -0500

    Day 5 final solution

--- p05.py
+++ p05.py
@@ -1,19 +1,12 @@
 from collections import deque
-# from more_itertools import chunked
+from more_itertools import chunked
 
-# from aocd import data, submit
+from aocd import data
 
 
 def overlap(r1, r2):
     return r1.stop > r2.start and r2.stop > r1.start
 
-
-def chunked(seq, n):
-    return zip(*[iter(seq)]*n)
-
-
-with open('5_real.in') as f:
-    data = f.read()
 
 seeds, *maps = data.split('\n\n')
 
@@ -24,36 +17,27 @@ for sr in seed_pairs:
     seed_ranges.append(range(sr[0], sr[0]+sr[1]))
 
 maps = [list(map(str.split, m))[1:] for m in  map(lambda s: s.split('\n'), maps)]
-# print(maps)
-# raise SystemExit
 
-# locations = []
+locations = []
 
-# for seed in seeds:
-#     n = int(seed)
-#     for m in maps:
-#         for r in m:
-#             dest, source, length = map(int, r)
-#             if n in range(source, source + length):
-#                 n = dest + (n - source)
-#                 break
+for seed in seeds:
+    n = int(seed)
+    for m in maps:
+        for r in m:
+            dest, source, length = map(int, r)
+            if n in range(source, source + length):
+                n = dest + (n - source)
+                break
 
-#     locations.append(n)
+    locations.append(n)
 
-# print(locations)
-# print(min(locations))
+print('Part 1:', min(locations))
 
-# import pudb;pu.db
-# print('maps:', len(maps))
 for i, m in enumerate(maps, 1):
-    print('current map:', i)
     new_seed_ranges = []
     for sr in seed_ranges:
         range_queue = deque([sr])
         while range_queue:
-            # print('range queue:', range_queue)
-            # if range_queue == deque([range(1585502931, 1729527586)]):
-            #     import pudb;pu.db
             cr = range_queue.popleft()
             for mr in m:
                 _, source, length = map(int, mr)
@@ -91,6 +75,4 @@ for i, m in enumerate(maps, 1):
         else:
             seed_ranges.append(sr)
 
-print(seed_ranges)
-print(min(seed_ranges, key=lambda r: r.start))
-print(min(seed_ranges, key=lambda r: r.start).start)
+print('Part 2:', min(seed_ranges, key=lambda r: r.start).start)
