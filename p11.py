2d25af4370ccc09a72e4f34105ab716867588029
Derek <sjukledighet@gmail.com>
2023-12-12 16:37:47 -0500

    Day 11 final solution

--- p11.py
+++ p11.py
@@ -1,17 +1,6 @@
 from itertools import combinations
 
-from aocd import data, submit
-
-# data = '''...#......
-# .......#..
-# #.........
-# ..........
-# ......#...
-# .#........
-# .........#
-# ..........
-# .......#..
-# #...#.....'''
+from aocd import data
 
 rows = data.splitlines()
 row_length = len(rows[0])
@@ -19,21 +8,6 @@ cols = list(zip(*rows))
 
 empty_rows = [i for i, row in enumerate(rows) if '#' not in row]
 empty_cols = [i for i, col in enumerate(cols) if '#' not in col]
-
-print(empty_rows, empty_cols)
-
-expanded = []
-for i, row in enumerate(rows):
-    if i in empty_rows:
-        expanded.append('.' * (row_length + len(empty_cols)))
-    new_row = ''
-    for j, col in enumerate(row):
-        if j in empty_cols:
-            new_row += '.'
-
-        new_row += col
-
-    expanded.append(new_row)
 
 galaxies = []
 
@@ -43,25 +17,30 @@ for y, row in enumerate(rows):
             galaxies.append((x, y))
 
 path_lengths = []
+path_lengths_very_expanded = []
 for g1, g2 in combinations(galaxies, 2):
     x1, y1 = g1
     x2, y2 = g2
 
     x_offset = 0
     y_offset = 0
+    big_x_offset = 0
+    big_y_offset = 0
 
     for i in empty_rows:
         if min(y1,y2) < i < max(y1,y2):
-            y_offset += int(1e6) - 1
-            # y_offset += 9
+            big_y_offset += int(1e6) - 1
+            y_offset += 1
 
     for i in empty_cols:
         if min(x1,x2) < i < max(x1,x2):
-            x_offset += int(1e6) - 1
-            # x_offset += 9
+            big_x_offset += int(1e6) - 1
+            x_offset += 1
 
     length = abs(x1 - x2) + abs(y1 - y2) + x_offset + y_offset
+    big_length = abs(x1 - x2) + abs(y1 - y2) + big_x_offset + big_y_offset
     path_lengths.append(length)
+    path_lengths_very_expanded.append(big_length)
 
-submit(sum(path_lengths))
-# print(sum(path_lengths))
+print('Part 1:', sum(path_lengths))
+print('Part 2:', sum(path_lengths_very_expanded))
