f602bb5d952080c2bfd4c7467952c27d3d2cc8ba
Derek <sjukledighet@gmail.com>
2023-12-20 02:38:17 -0500

    Day 18 part 2

--- p18.py
+++ p18.py
@@ -1,4 +1,5 @@
 from collections import deque
+from shapely import Polygon
 
 from aocd import data, submit
 
@@ -18,13 +19,32 @@ from aocd import data, submit
 # U 2 (#7a21e3)'''
 
 x = y = 0
+rx = ry = 0
 lagoon = set()
 min_x = min_y = float('inf')
 max_x = max_y = float('-inf')
+coords = [(0, 0)]
+real_coords = [(0, 0)]
+real_perimeter = 0
 
 for line in data.splitlines():
     direction, amount, hex_value = line.split()
     amount = int(amount)
+
+    real_amount = int(hex_value[2:-2], 16)
+    real_direction = hex_value[-2]
+    real_perimeter += real_amount
+
+    if real_direction == '0':    # R
+        rx += real_amount
+    elif real_direction == '1':  # D
+        ry += real_amount
+    elif real_direction == '2':  # L
+        rx -= real_amount
+    elif real_direction == '3':  # U
+        ry -= real_amount
+
+    real_coords.append((rx, ry))
 
     for _ in range(amount):
         if direction == 'R':
@@ -41,6 +61,8 @@ for line in data.splitlines():
         max_x = max(max_x, x)
         min_y = min(min_y, y)
         max_y = max(max_y, y)
+
+    coords.append((x, y))
 
 queue = deque([(min_x-1, min_y-1)])
 seen = {(min_x-1, min_y-1)}
@@ -62,4 +84,12 @@ while queue:
 
 bounding_box_area = (abs((min_x - 1) - (max_x + 1)) + 1) * (abs((min_y - 1) - (max_y + 1)) + 1)
 lagoon_area = bounding_box_area - len(seen)
-submit(lagoon_area)
+print('Part 1:', lagoon_area)
+# assert lagoon_area == 50465
+
+polygon = Polygon(coords)
+print(polygon.area + len(lagoon) // 2 + 1)
+
+real_polygon = Polygon(real_coords)
+real_area = int(real_polygon.area + real_perimeter // 2 + 1)
+submit(real_area)
